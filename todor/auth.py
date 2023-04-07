ad7ef9d083e1e360065c2d2f130f630fa4cab19e
luis-pablo <pabloarias024@gmail.com>
2023-04-07 11:16:40 -0400

    login y base de datos funcioando

--- todor/auth.py
+++ todor/auth.py
@@ -1,10 +1,31 @@
-from flask import Blueprint, render_template
-from . import models
+from flask import (Blueprint, render_template, request, url_for, redirect, flash)
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .models import User
+from todor import db
 
 bp = Blueprint('auth', __name__, url_prefix ='/auth')
 
-@bp.route('/register')
+@bp.route('/register', methods= ('GET', 'POST'))
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        error = None
+        
+        user = User(username, generate_password_hash(password))
+        
+        user_name = User.query.filter_by(username = username).first()
+        if user_name == None:
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('auth.login'))
+        else: 
+            return f'El usuario {user_name} ya esta registrado'
+        flash(error)
+        
     return render_template('auth/register.html')
 
 @bp.route('/login')
