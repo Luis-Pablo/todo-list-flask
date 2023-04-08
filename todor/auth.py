6c35f12f37b084596bf25b9c723deed4a909eaeb
luis-pablo <pabloarias024@gmail.com>
2023-04-07 23:51:22 -0400

    login funcionando

--- todor/auth.py
+++ todor/auth.py
@@ -1,5 +1,5 @@
-from flask import (Blueprint, render_template, request, url_for, redirect, flash)
-
+from flask import (Blueprint, render_template, request, url_for, redirect, flash, session, g)
+# el objeto g se utiliza para guardar un valor, para despues usarlo en cualquier parte de al app, ej una cookie, etc
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from .models import User
@@ -13,10 +13,11 @@ def register():
         username = request.form['username']
         password = request.form['password']
         
-        error = None
+        
         
         user = User(username, generate_password_hash(password))
         
+        error = None
         user_name = User.query.filter_by(username = username).first()
         if user_name == None:
             db.session.add(user)
@@ -28,6 +29,52 @@ def register():
         
     return render_template('auth/register.html')
 
-@bp.route('/login')
+
+@bp.route('/login', methods= ('GET', 'POST'))
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        error = None
+        #validar datos        
+        user = User.query.filter_by(username = username).first()
+        if user == None:
+            error = "Nombre de usuario incorrecto"
+        elif not check_password_hash(user.password, password):
+            error = "Contraseña incorrecta"
+            
+        #iniciar sesion
+        if error is None:
+            session.clear()
+            session['user_id'] = user.id
+            return redirect(url_for('todo.index'))
+        
+
+        flash(error)
+        
     return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get_or_404(user_id)
+        
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+import functools
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
--- todor/todo.py
+++ todor/todo.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint('todo', __name__, url_prefix ='/todo')
 
 @bp.route('/list')
-def list():
-    return "Lista de tareas"
+def index():
+    return render_template('todo/index.html')
 
 @bp.route('/create')
 def create():
